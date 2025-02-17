bd235a8ab6eae61bd48664da754243ab8d44faa0
Richaard Lipa <rlipac31@gmail.com>
2025-02-16 19:18:50 -0500

    buscando corregir error font.ts de next

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,5 @@
-import { Inter, Montserrat, Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
  
 export const inter = Inter({ subsets: ['latin'] });
-export const montserrat = Montserrat({subsets: ['latin'] });
 export const lusitana  = Lusitana( { weight: ['400']});
